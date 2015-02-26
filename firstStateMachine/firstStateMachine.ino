9fbd9a4d24c7f987057137245cdfdc2f37a0face
Bastiaan Weijers <bastiaanweijers@gmail.com>
2015-02-26 09:55:10 +0100

    magnetstates

--- firstStateMachine/firstStateMachine.ino
+++ firstStateMachine/firstStateMachine.ino
@@ -89,6 +89,7 @@ unsigned long maxStandTime = 0;
 unsigned long maxUseTime = 0;
 unsigned long previousDistState = 0;
 unsigned long fleeAttemptTime = 0;
+unsigned long magnetTime = 0;
 
 int sitDistance = 35;
 int standDistance = 80;
@@ -104,6 +105,7 @@ bool magnet = true;
 bool movingState;
 bool menuActive = false;
 
+bool magnetize = false;
 
 int motionTimer = 0;
 #include <LiquidCrystal.h>
@@ -140,7 +142,7 @@ void setup() {
   pinMode(ledPin, OUTPUT); // Use LED indicator (if required)
   pinMode(LDRPin, INPUT);
   pinMode(magnetPin, INPUT);
-  
+
   lcd.setCursor(0, 0);
   lcd.print("Sprays: ");
   amountOfSpraysLeft = readEEPROM();
@@ -148,10 +150,28 @@ void setup() {
 }
 
 void loop() {
+  
+
   movingState = digitalRead(motionPin);
   magnetState = digitalRead(magnetPin);
 
   unsigned long currentMillis = millis();
+
+  if (magnetState == HIGH && magnetize == false && (currentMillis - magnetTime) > 500){
+    magnetize = true;
+    magnetTime = currentMillis;
+    nr1Timer = currentMillis;
+    sprayButTimer = currentMillis;
+    lcd.setCursor(13,0);
+    lcd.print("ON");
+  }
+  else if(currentMillis - magnetTime > 30000 && !inUse && !sprayButtonTouched){
+    magnetize = false;
+    magnetTime = currentMillis;
+    lcd.setCursor(13,0);
+    lcd.print("  ");
+  }
+
 
   spraying(currentMillis);
 
@@ -195,9 +215,12 @@ void loop() {
         EEPROM.write(15, sprayDelay / 1000);
         if (rb[2] == 0) {
           resetEEPROM(2500);//eepromWrite(2243);
-          readEEPROM();          
+          amountOfSpraysLeft = readEEPROM();
         }
       }
+      lcd.setCursor(0, 0);
+      lcd.print("Sprays: ");
+      lcd.print(amountOfSpraysLeft);
     }
   }
   if (menuActive && choiceButton.read()) {
@@ -256,7 +279,7 @@ void loop() {
 
       sensors.requestTemperatures(); // Send the command to get temperatures
 
-      // Loop through each device, print out temperature data
+        // Loop through each device, print out temperature data
       for (int i = 0; i < numberOfDevices; i++)
       {
         // Search the wire for address
@@ -266,13 +289,13 @@ void loop() {
 
           // It responds almost immediately. Let's print out the data
           printTemperature(tempDeviceAddress); // Use a simple function to print out the data
-          
-            
-            float tempC = sensors.getTempC(tempDeviceAddress);
-            lcd.setCursor(0, 1);
-            lcd.print("Temp C: ");
-            lcd.print(tempC);
-          
+
+
+          float tempC = sensors.getTempC(tempDeviceAddress);
+          lcd.setCursor(0, 1);
+          lcd.print("Temp C: ");
+          lcd.print(tempC);
+
 
         }
         //else ghost device! Check your power requirements and cabling
@@ -306,7 +329,7 @@ void checkDistanceState(int dist) {
       else if (attemptToUse && !far && useAttemptTime >= decisionTime) {
         inUse = true;
         greenOn = true;
-        digitalWrite(greenLED, HIGH);
+        //digitalWrite(greenLED, HIGH);
         digitalWrite(redLED, LOW);
         useTime = 0;
         standing = true;
@@ -324,281 +347,294 @@ void checkDistanceState(int dist) {
     else if (standing) {
       bool sitClose = (dist <= sitDistance);
       bool standClose = (dist <= standDistance);
-      
-      if (current - greenTimer > 500) {
-            if (greenOn) {
-              digitalWrite(greenLED, LOW);
-              greenOn = false;
-              digitalWrite(redLED, LOW);
-              redOn = false;
 
-            }
-            else {
-              digitalWrite(greenLED, HIGH);
-              greenOn = true;
-              digitalWrite(redLED, HIGH);
-              redOn = true;
-            }
-            greenTimer = current;
+      if (current - greenTimer > 500) {
+        if (greenOn) {
+          digitalWrite(greenLED, LOW);
+          greenOn = false;
+          digitalWrite(redLED, LOW);
+          redOn = false;
+
         }
-        
+        else {
+          digitalWrite(greenLED, HIGH);
+          greenOn = true;
+          digitalWrite(redLED, HIGH);
+          redOn = true;
+        }
+        greenTimer = current;
+      }
+
       if (standClose && !sitClose) {
         standTime += timeDif;
         useTime += timeDif;
       }
-        
 
+
+      else {
+        if (!sitting && sitClose && !attemptToSit) {
+          attemptToSit = true;
+          sitAttemptTime = 0;
+        }
+        else if (attemptToSit && sitClose && sitAttemptTime < decisionTime) {
+          sitAttemptTime += timeDif;
+        }
+
+        else if (attemptToSit && sitClose && sitAttemptTime >= decisionTime) {
+          sitting = true;
+          maxStandTime = max(maxStandTime, standTime);
+          standing = false;
+          standTime = 0;
+          sitTime = 0;
+          attemptToSit = false;
+        }
         else {
-          if (!sitting && sitClose && !attemptToSit) {
-            attemptToSit = true;
-            sitAttemptTime = 0;
-          }
-          else if (attemptToSit && sitClose && sitAttemptTime < decisionTime) {
-            sitAttemptTime += timeDif;
-          }
-
-          else if (attemptToSit && sitClose && sitAttemptTime >= decisionTime) {
-            sitting = true;
-            maxStandTime = max(maxStandTime, standTime);
-            standing = false;
-            standTime = 0;
-            sitTime = 0;
-            attemptToSit = false;
-          }
-          else {
-            sitAttemptTime = 0;
-          }
+          sitAttemptTime = 0;
         }
+      }
+
+    }
+
+    else if (sitting) {
+      bool sitClose = (dist <= sitDistance);
+      bool standClose = (dist <= standDistance);
+
+      if (current - greenTimer > 1500) {
+        if (greenOn) {
+          digitalWrite(greenLED, LOW);
+          greenOn = false;
+          digitalWrite(redLED, LOW);
+          redOn = false;
+
+        }
+        else {
+          digitalWrite(greenLED, HIGH);
+          greenOn = true;
+          digitalWrite(redLED, HIGH);
+          redOn = true;
+        }
+        greenTimer = current;
+      }
+
+      if (sitClose) {
+        sitTime += timeDif;
+        useTime += timeDif;
 
       }
 
-      else if (sitting) {
-        bool sitClose = (dist <= sitDistance);
-        bool standClose = (dist <= standDistance);
-        
-        if (current - greenTimer > 1500) {
-            if (greenOn) {
-              digitalWrite(greenLED, LOW);
-              greenOn = false;
-              digitalWrite(redLED, LOW);
-              redOn = false;
-
-            }
-            else {
-              digitalWrite(greenLED, HIGH);
-              greenOn = true;
-              digitalWrite(redLED, HIGH);
-              redOn = true;
-            }
-            greenTimer = current;
+      else {
+        if (!sitClose && standClose && !attemptToStand) {
+          attemptToStand = true;
+          standAttemptTime = 0;
         }
-
-        if (sitClose) {
-          sitTime += timeDif;
-          useTime += timeDif;
-          
-          }
-
-          else {
-            if (!sitClose && standClose && !attemptToStand) {
-              attemptToStand = true;
-              standAttemptTime = 0;
-            }
-            else if (!sitClose && standClose && attemptToStand && standAttemptTime < decisionTime) {
-              standAttemptTime += timeDif;
-            }
-            else if (!sitClose && standClose && attemptToStand && standAttemptTime >= decisionTime) {
-              standing = true;
-              sitting = false;
-              maxSitTime = max(maxSitTime, sitTime);
-              sitTime = 0;
-              standTime = 0;
-              attemptToStand = false;
-            }
-            else {
-              standAttemptTime = 0;
-            }
-          }
+        else if (!sitClose && standClose && attemptToStand && standAttemptTime < decisionTime) {
+          standAttemptTime += timeDif;
         }
-
-
-        if (far && inUse && current - lastUsed >= usageDelay) {
-          if (!attemptToFlee && far) {
-            attemptToFlee = true;
-            fleeAttemptTime = 0;
-          }
-          else if (attemptToFlee && fleeAttemptTime < decisionTime && far) {
-            fleeAttemptTime += timeDif;
-          }
-          else if (attemptToFlee && fleeAttemptTime >= decisionTime && far) {
-            inUse = false;
-
-            if (standing) {
-              maxStandTime = max(maxStandTime, standTime);
-            }
-            if (sitting) {
-              maxSitTime = max(maxSitTime, sitTime);
-            }
-            standing = false;
-            sitTime = 0;
-            standTime = 0;
-            sitting = false;
-            maxUseTime = max(maxUseTime, useTime);
-            useTime = 0;
-            lastUsed = current;
-            digitalWrite(redLED, LOW);
-            digitalWrite(greenLED, HIGH);
-            usageDone(maxUseTime, maxStandTime, maxSitTime);
-            maxUseTime = 0;
-            maxStandTime = 0;
-            maxSitTime = 0;
-          }
+        else if (!sitClose && standClose && attemptToStand && standAttemptTime >= decisionTime) {
+          standing = true;
+          sitting = false;
+          maxSitTime = max(maxSitTime, sitTime);
+          sitTime = 0;
+          standTime = 0;
+          attemptToStand = false;
         }
-
+        else {
+          standAttemptTime = 0;
+        }
       }
+    }
+
+
+    if (far && inUse && current - lastUsed >= usageDelay) {
+      if (!attemptToFlee && far) {
+        attemptToFlee = true;
+        fleeAttemptTime = 0;
+      }
+      else if (attemptToFlee && fleeAttemptTime < decisionTime && far) {
+        fleeAttemptTime += timeDif;
+      }
+      else if (attemptToFlee && fleeAttemptTime >= decisionTime && far) {
+        inUse = false;
+
+        if (standing) {
+          maxStandTime = max(maxStandTime, standTime);
+        }
+        if (sitting) {
+          maxSitTime = max(maxSitTime, sitTime);
+        }
+        standing = false;
+        sitTime = 0;
+        standTime = 0;
+        sitting = false;
+        maxUseTime = max(maxUseTime, useTime);
+        useTime = 0;
+        lastUsed = current;
+        digitalWrite(redLED, LOW);
+
+        usageDone(maxUseTime, maxStandTime, maxSitTime);
+        maxUseTime = 0;
+        maxStandTime = 0;
+        maxSitTime = 0;
+      }
+    }
+
+  }
 
 
 
-    
-  
+
+
 }
 
-  // function to print the temperature for a device
-  void printTemperature(DeviceAddress deviceAddress)
-  {
-    // method 1 - slower
-    //Serial.print("Temp C: ");
-    //Serial.print(sensors.getTempC(deviceAddress));
-    //Serial.print(" Temp F: ");
-    //Serial.print(sensors.getTempF(deviceAddress)); // Makes a second call to getTempC and then converts to Fahrenheit
+// function to print the temperature for a device
+void printTemperature(DeviceAddress deviceAddress)
+{
+  // method 1 - slower
+  //Serial.print("Temp C: ");
+  //Serial.print(sensors.getTempC(deviceAddress));
+  //Serial.print(" Temp F: ");
+  //Serial.print(sensors.getTempF(deviceAddress)); // Makes a second call to getTempC and then converts to Fahrenheit
 
-    // method 2 - faster
-    float tempC = sensors.getTempC(deviceAddress);
-    Serial.print("Temp C: ");
-    Serial.print(tempC);
-    Serial.print(" Temp F: ");
-    Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+  // method 2 - faster
+  float tempC = sensors.getTempC(deviceAddress);
+  Serial.print("Temp C: ");
+  Serial.print(tempC);
+  Serial.print(" Temp F: ");
+  Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+}
+
+void usageDone(unsigned long maxUse, unsigned long maxStand, unsigned long maxSit) {
+  //lcd.clear();
+  //lcd.setCursor(0, 0);
+
+  if (maxUse < minimumTime) {
+    //lcd.print("Nothing");
+    digitalWrite(greenLED, LOW);
+  }
+  else if (maxSit <= nr1Time) {
+    //lcd.print("Spray: 1 time");
+    nr1Done = true;
+    nr1Timer = millis();
+    sprayButtonTouched = true;
+    sprayButTimer = millis();
+  }
+  else if (maxSit > nr1Time) {
+    //lcd.print("Spray: 2 times");
+    nr2Done = true;
+    nr2Timer = millis();
+    sprayButtonTouched = true;
+    sprayButTimer = millis();
   }
 
-  void usageDone(unsigned long maxUse, unsigned long maxStand, unsigned long maxSit) {
+
+}
+
+void spraying(long currentMillis) {
+  if (sprayButton.read() && currentMillis > 500) {
     //lcd.clear();
     //lcd.setCursor(0, 0);
+    //lcd.print("Spraying in");
+    nr1Done = true;
+    nr1Timer = currentMillis;
+    sprayButtonTouched = true;
+    magnetize = true;
+    sprayButTimer = currentMillis;
+  }
+  if(currentMillis - magnetTime < 10000 && magnetize && nr1Done){
+    sprayButtonTouched = true;
+  }
 
-    if (maxUse < minimumTime) {
-      //lcd.print("Nothing");
+  if (currentMillis - sprayButTimer > sprayDelay) {
+    sprayButtonTouched = false;
+  }
+  else if (sprayButtonTouched && magnetize) {
+    digitalWrite(greenLED, HIGH);
+    lcd.setCursor(0, 0);
+    lcd.print("Spraying in  ");
+    lcd.setCursor(0, 1);
+    lcd.print("seconds: ");
+    lcd.print("~");
+    lcd.print((sprayDelay  - (currentMillis - sprayButTimer)) / 1000);
+    lcd.print("  ");
+  }
+  if (nr1Done && currentMillis - nr1Timer > sprayDelay && magnetize) {
+    //digitalWrite(greenLED, HIGH);
+  }
+
+  if (nr1Done && currentMillis - nr1Timer > sprayDelay - ledDelay && magnetize) {
+    digitalWrite(ledPin, HIGH);
+    nr1Done = false;
+    nr1Light = true;
+  }
+
+  if ((nr1Light && currentMillis - nr1Timer > sprayDelay + extraDelay) || (nr2Light && currentMillis - nr2Timer > sprayDelay + extraDelay && magnetize)) {
+    digitalWrite(ledPin, LOW);
+    if (nr2Light) {
+      nr1Done = true;
+      nr2Light = false;
+      nr1Timer = currentMillis;
+    }
+    else {
+      nr1Light = false;
       digitalWrite(greenLED, LOW);
     }
-    else if (maxSit <= nr1Time) {
-      //lcd.print("Spray: 1 time");
-      nr1Done = true;
-      nr1Timer = millis();
-      sprayButtonTouched = true;
-      sprayButTimer = millis();
-    }
-    else if (maxSit > nr1Time) {
-      //lcd.print("Spray: 2 times");
-      nr2Done = true;
-      nr2Timer = millis();
-      sprayButtonTouched = true;
-      sprayButTimer = millis();
-    }
 
+    amountOfSpraysLeft = readEEPROM() - 1;
+    writeEEPROM(amountOfSpraysLeft);
+
+    //lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Sprays: ");
+    lcd.print(amountOfSpraysLeft);
+    digitalWrite(redLED, LOW);
+    magnetize = false;
+    lcd.setCursor(13,0);
+    lcd.print("  ");
 
   }
 
-  void spraying(long currentMillis) {
-    if (sprayButton.read() && currentMillis > 500) {
-      //lcd.clear();
-      //lcd.setCursor(0, 0);
-      //lcd.print("Spraying in");
-      nr1Done = true;
-      nr1Timer = currentMillis;
-      sprayButtonTouched = true;
-      sprayButTimer = currentMillis;
-    }
+  if (nr2Done && currentMillis - nr2Timer > sprayDelay - ledDelay) {
+    nr2Light = true;
+    nr2Done = false;
+    digitalWrite(ledPin, HIGH);
+  }
 
-    if (currentMillis - sprayButTimer > sprayDelay) {
-      sprayButtonTouched = false;
-    }
-    else if (sprayButtonTouched) {
-      lcd.setCursor(0, 0);
-      lcd.print("Spraying in ");
-      lcd.setCursor(0, 1);
-      lcd.print("seconds: ");
-      lcd.print("~");
-      lcd.print((sprayDelay  - (currentMillis - sprayButTimer)) / 1000);
-      lcd.print("  ");
-    }
-
-    if (nr1Done && currentMillis - nr1Timer > sprayDelay - ledDelay) {
-      digitalWrite(ledPin, HIGH);
-      nr1Done = false;
-      nr1Light = true;
-    }
-
-    if ((nr1Light && currentMillis - nr1Timer > sprayDelay + extraDelay) || (nr2Light && currentMillis - nr2Timer > sprayDelay + extraDelay)) {
-      digitalWrite(ledPin, LOW);
-      if (nr2Light) {
-        nr1Done = true;
-        nr2Light = false;
-        nr1Timer = currentMillis;
-      }
-      else {
-        nr1Light = false;
-        digitalWrite(greenLED, LOW);
-      }
-
-      amountOfSpraysLeft = readEEPROM() - 1;
-      writeEEPROM(amountOfSpraysLeft);
-
-      //lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Sprays: ");
-      lcd.print(amountOfSpraysLeft);
-      digitalWrite(redLED, LOW);
-      
-
-    }
-
-    if (nr2Done && currentMillis - nr2Timer > sprayDelay - ledDelay) {
-      nr2Light = true;
-      nr2Done = false;
-      digitalWrite(ledPin, HIGH);
-    }
-  
 }
 
 
 void writeEEPROM(int value){
-	
-	int i = EEPROM.read(11);
-	int curval = EEPROM.read(i);
-	if (curval == 1){
-		EEPROM.write(11, i-1);
-		writeEEPROM(value);
-	}else{
-		EEPROM.write(i, curval-1);
-	}
+
+  int i = EEPROM.read(11);
+  int curval = EEPROM.read(i);
+  if (curval == 1){
+    EEPROM.write(11, i-1);
+    writeEEPROM(value);
+  }
+  else{
+    EEPROM.write(i, curval-1);
+  }
 }
 
 void resetEEPROM(int value){
-	
-	for (int n=0; n<10; n++){
-		if (value <= 255){
-			EEPROM.write(n, value);
-		}else{
-			EEPROM.write(n, 255);
-		}
-                value-= 255;
-	}
-	EEPROM.write(11, 9);
+
+  for (int n=0; n<10; n++){
+    if (value <= 255){
+      EEPROM.write(n, value);
+    }
+    else{
+      EEPROM.write(n, 255);
+    }
+    value-= 255;
+  }
+  EEPROM.write(11, 9);
 }
 
 int readEEPROM(){
-	int value = 0;
-	int i = EEPROM.read(11);
-        for (int n=0; n<=i; n++){
-          value += EEPROM.read(n);
-        }
-	return value;
+  int value = 0;
+  int i = EEPROM.read(11);
+  for (int n=0; n<=i; n++){
+    value += EEPROM.read(n);
+  }
+  return value;
 }
+
