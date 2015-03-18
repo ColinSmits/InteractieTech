9d0a9d21a0fbb6c30a6125b9d374c67a1855612e
Colin Smits <colin1996@hotmail.nl>
2015-03-18 10:28:23 +0100

    First working prototype
    Swipen werkt voor iemand met korte mouwen, halve meter afstand

--- pract2/functions.py
+++ pract2/functions.py
@@ -4,13 +4,15 @@ import numpy as np
 def skinFilter(frame, lower, upper):
 	converted = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	skinMask = cv2.inRange(converted, lower, upper)
-	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-	skinMask = cv2.erode(skinMask, kernel, iterations = 3)
-	skinMask = cv2.dilate(skinMask, kernel, iterations = 3)
+	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+	skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+	skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
 	skinMask = cv2.GaussianBlur(skinMask, (3,3), 0)
 	skin = cv2.bitwise_and(frame, frame, mask = skinMask)
 	skin = cv2.cvtColor(skin, cv2.COLOR_RGB2GRAY)
 	ret, tresh1 = cv2.threshold(skin, 0,255,cv2.THRESH_BINARY)
+	cv2.imshow("binary", tresh1)
+
 	center = findDefects(frame, tresh1)
 	
 	return [frame, center]
@@ -22,7 +24,9 @@ def findDefects(frame, gray):
 	ci = 0
 	
 	n = 0
-	if (contours):
+	cx = None
+	cy = None
+	if len(contours) > 1:
 		for i in range(len(contours)):
 			cnt = contours[n]
 			#if cv2.isContourConvex(cnt):
@@ -40,35 +44,51 @@ def findDefects(frame, gray):
 		#print str(contours)
 		cnt = contours[ci]
 		
-		M = cv2.moments(cnt)
-		cx = int(M['m10']/M['m00'])
-		cy = int(M['m01']/M['m00'])
-		#print len(contours)
-		#print ci
-		#print str(cnt)
-		hull = cv2.convexHull(cnt,returnPoints = False)
-		#print str(hull)
-		defects = cv2.convexityDefects(cnt,hull)
-		#print str(defects)
+		if maxArea > 10000:
+			M = cv2.moments(cnt)
+			cx = int(M['m10']/M['m00'])
+			cy = int(M['m01']/M['m00'])
+			#print len(contours)
+			#print ci
+			#print str(cnt)
+			hull = cv2.convexHull(cnt,returnPoints = False)
+			#print str(hull)
+			defects = cv2.convexityDefects(cnt,hull)
+			#print str(defects)
 
-		for i in range(defects.shape[0]):
-			s,e,f,d = defects[i,0]
-			start = tuple(cnt[s][0])
-			end = tuple(cnt[e][0])
-			far = tuple(cnt[f][0])
-			#print start
-			#print end
-			#print far
-			cv2.line(frame,start,end,[0,255,0],2)
-			cv2.circle(frame,far,5,[0,0,255],-1)
+			for i in range(defects.shape[0]):
+				s,e,f,d = defects[i,0]
+				start = tuple(cnt[s][0])
+				end = tuple(cnt[e][0])
+				far = tuple(cnt[f][0])
+				#print start
+				#print end
+				#print far
+				cv2.line(frame,start,end,[0,255,0],2)
+				cv2.circle(frame,far,5,[0,0,255],-1)
 
 	return (cx, cy)
 			
 
+def processHand(center, prev_center, prev_Offset):
+	
+
+	difference_X = center[0] - prev_center[0]
+	difference_y = center[1] - prev_center[1]
+
+	off_X = prev_Offset[0] + difference_X
+	off_Y = prev_Offset[1] + difference_y
+
+	return (off_X, off_Y) 
 
 
 
+def getLength(vector):
+	return (vector[0]**2 + vector[1]**2)**0.5
 
 
+def getLengthX(vector):
+	return vector[0]
 
-
+def getLengthY(vector):
+	return vector[1]
--- pract2/testBackground.py
+++ pract2/testBackground.py
@@ -2,6 +2,13 @@ import numpy as np
 import cv2
 import functions
 
+import win32com.client as comclt
+import win32api
+
+# Initialiseer de Windows shell in Python zodat je naar de shell kunt scripten
+wsh= comclt.Dispatch("WScript.Shell")
+
+
 cap = cv2.VideoCapture(0)
 fgbg = cv2.BackgroundSubtractorMOG()
 
@@ -9,8 +16,8 @@ fgbg = cv2.BackgroundSubtractorMOG()
 #frame = cv2.imread("test2OtherLighting.jpg")
 #skin filter boundaries (tweak if necessary)
 lower = np.array([0, 45, 80], dtype = "uint8")
-upper = np.array([11, 255, 255], dtype = "uint8")
-
+upper = np.array([15, 255, 255], dtype = "uint8")
+handFound = False
 
 
 #"""
@@ -20,17 +27,66 @@ while (True):
     if (not frame1 == None):
         break
 
+lastFrame = 0
+lastFound = 0
+findDelay = 20
+maxDelay = 20
+
+trigger = 400
+
 #cam started, process frames
 while(cap.isOpened):
     ret, frame = cap.read()
 
     if ret == True:
-#"""
 
-		img = frame.copy()
-		#back = fgbg.apply(img)
-		img = functions.skinFilter(img, lower, upper)
-		cv2.imshow("images", img)
+        if lastFound < findDelay:
+            lastFound += 1
+
+        img = frame.copy()
+       # back = fgbg.apply(img)
+       # img = cv2.bitwise_and(img, img, mask=back)
+       # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+       # img = cv2.erode(img, kernel, iterations = 2)
+       # img = cv2.dilate(img, kernel, iterations = 2)
+        listImg = functions.skinFilter(img, lower, upper)
+        img = listImg[0]
+        center = listImg[1]
+        cv2.imshow("images", img)
+
+        if (center[0] != None):
+            if (handFound == True):
+                prev_Offset = functions.processHand(center, prev_center, prev_Offset)
+                
+                length = functions.getLengthX(prev_Offset)
+                if abs(length) > trigger:
+                    print length
+                    if length < 0:
+                        wsh.AppActivate("Photo Gallery") # select another application
+                        wsh.sendKeys("{Left}")
+                    else:
+                        wsh.AppActivate("Photo Gallery")
+                        wsh.sendKeys("{Right}")
+
+                    handFound = False
+                    lastFound = 0
+
+
+            elif lastFound >= findDelay:
+                handFound = True
+                prev_Offset = (0,0)
+                prev_center = center
+
+            lastFrame = 0
+
+        else:
+            lastFrame += 1
+
+            if lastFrame > maxDelay:
+                handFound == True
+
+
+
 
 
 #"""      
