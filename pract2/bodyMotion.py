8d64b48b50e256dd226bd7cb492bf568cb7b5c29
Colin Smits <colin1996@hotmail.nl>
2015-03-19 14:28:58 +0100

    commented

--- pract2/bodyMotion.py
+++ pract2/bodyMotion.py
@@ -5,22 +5,20 @@ import functions
 import win32com.client as comclt
 import win32api
 
-# Initialiseer de Windows shell in Python zodat je naar de shell kunt scripten
+# Initialise Windows shell for communication
 wsh= comclt.Dispatch("WScript.Shell")
 
 
 cap = cv2.VideoCapture(0)
-fgbg = cv2.BackgroundSubtractorMOG()
+#fgbg = cv2.BackgroundSubtractorMOG()
 
-
-#frame = cv2.imread("test2OtherLighting.jpg")
 #skin filter boundaries (tweak if necessary)
 lower = np.array([0, 144, 77], dtype = "uint8")
 upper = np.array([255, 173, 127], dtype = "uint8")
 handFound = False
 
 
-#"""
+
 #starting cam: no frame yet
 while (True):
     ret, frame1 = cap.read()
@@ -46,6 +44,7 @@ while(cap.isOpened):
 
     if ret == True:
 
+        # Delays in order to make sure a gesture will not be processed twice at once
         if lastFound < findDelay:
             lastFound += 1
 
@@ -58,7 +57,9 @@ while(cap.isOpened):
             twoHands = False
             _2HandsFound = False
 
+
         img = frame.copy()
+
         """
         back = fgbg.apply(img)
         img = cv2.bitwise_and(img, img, mask=back)
@@ -66,7 +67,12 @@ while(cap.isOpened):
         img = cv2.erode(img, kernel, iterations = 2)
         img = cv2.dilate(img, kernel, iterations = 2)
         """
+
+        # process the image to find skin locations
+        # Returns list: [ img, centers ] where centers = [(int, int)]
         listImg = functions.skinFilter(img, lower, upper)
+
+
         img = listImg[0]
         centers = listImg[1]
         
@@ -75,8 +81,7 @@ while(cap.isOpened):
         cv2.imshow("img", img)
         
 
-        
-    
+        # one hand found 
         if len(centers) == 1 and not twoHands:
             
             if (handFound == True):
@@ -84,28 +89,34 @@ while(cap.isOpened):
                 prev_centers = centers
                 length = functions.getLengthX(prev_Offset)
                 if abs(length) > trigger:
-                    #print length
-                    if length < 0:
-                        wsh.AppActivate("Photo Gallery") # select another application
+                    # put Photo Gallery up as active
+                    # send the right key 
+                    if length < 0: 
+                        # previous
+                        wsh.AppActivate("Photo Gallery")
                         wsh.sendKeys("{Left}")
                     else:
+                        # next 
                         wsh.AppActivate("Photo Gallery")
                         wsh.sendKeys("{Right}")
 
+                    # change state and start delay of recognition
                     handFound = False
                     lastFound = 0
 
-
+            # change state when not found hand before
             elif lastFound >= findDelay:
                 handFound = True
                 prev_Offset = (0,0)
                 prev_centers = centers
 
+            # make sure state is right
             lastFrame = 0
             oneHand = True
 
 
 
+        # 2 hands found
         elif len(centers) == 2:
             
             twoHands = True
@@ -113,30 +124,33 @@ while(cap.isOpened):
             lengths = dict()
             if (_2HandsFound == True):
                 for center in centers:
-                    #print len(prev_centers)
                     related.append(functions.getPreviousCenter(center, prev_centers))
-                    #print related
+                    
+                # check whether right centers are appointed
+                """
+                It's possible this returns the wrong centers. 
+                This can only happen when the distance of both is more or less the same. 
+                For offset this doesn't matter that much.
+                """
 
                 if related[0] == related[1]:
                     print "Kan niet"
 
+                # if centers are reversed: reverse the centers
                 if related[0] == 1:
                     centers.reverse()
 
-                #print centers
+                # for each center: update the current total offset
                 for i in range(len(centers)):
-                    print related[i]
-                    print len(centers)
+                    
                     prev_Offsets[i] = functions.processHand(centers[i], prev_centers[related[i]], prev_Offsets[i])
                     lengths[i] = functions.getLengthY(prev_Offsets[i])
 
-                
-
-                
+                # if both offsets are large enough, trigger recognition      
                 if abs(lengths[0]) > trigger2Hands and abs(lengths[1]) > trigger2Hands:
-                    #print lengths
+                    
 
-                    # check which is left and which is right
+                    # check which is left and which is right in order for roation to
                     if centers[0][0] < centers[1][0]:
 
                         if lengths[0] < 0 and lengths[1] > 0:
@@ -164,10 +178,16 @@ while(cap.isOpened):
                             wsh.sendKeys("^.")
                             _2HandsFound = False
                             lastFound2 = 0
+                    
+                    # nog testen!!
+                    """
+                    else:
+                        prev_Offsets = [(0,0), (0,0)]
+                    """
 
                 prev_centers = centers
 
-
+            # first time when two hands are recognised
             elif lastFound2 >= findDelay:
                 _2HandsFound = True
                 prev_Offsets = [(0,0), (0,0)]
@@ -175,36 +195,18 @@ while(cap.isOpened):
 
             lastFrame2 = 0
 
-
-
-
-
-
-
-
-
-
         else:
             lastFrame += 1
 
             if lastFrame > maxDelay:
                 handFound == False
                 lastFrame = 0
-
-
-
-
-
-
     
     k = cv2.waitKey(30) & 0xff
     if k == 27:
        break
 
 cap.release()
-"""
-cv2.waitKey(0)
-"""
 cv2.destroyAllWindows()
 
 
--- pract2/functions.py
+++ pract2/functions.py
@@ -1,24 +1,41 @@
 import cv2
 import numpy as np
 
+
+# Function to detect skin patches and identify their location
 def skinFilter(frame, lower, upper):
+
+	# convert frame to YCrCb from BGR (reversed RGB)
 	converted = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
+
+	#filter the pixels within the skin color range
 	skinMask = cv2.inRange(converted, lower, upper)
+
+	# process the frame (erosion, dilation) for better results
 	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
 	skinMask = cv2.erode(skinMask, kernel, iterations = 2)
 	skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
 	skinMask = cv2.GaussianBlur(skinMask, (3,3), 0)
+
+	# Use AND operator to identify pixels that can be used
 	skin = cv2.bitwise_and(frame, frame, mask = skinMask)
+
+	#Threshold and create binary image for processing
 	skin = cv2.cvtColor(skin, cv2.COLOR_RGB2GRAY)
 	ret, tresh1 = cv2.threshold(skin, 0,255,cv2.THRESH_BINARY)
 	cv2.imshow("binary", tresh1)
 
+	#calculate the centers of the skin patches by identifying contours
 	centers = findDefects(frame, tresh1)
 	
 	return [frame, centers]
 
 
+
+# Function to find the defects and contours in the frame
 def findDefects(frame, gray):
+
+	# find the contours
 	contours,hierarchy = cv2.findContours(gray, 1, 2)
 	maxArea = 0
 	ci = 0
@@ -29,6 +46,8 @@ def findDefects(frame, gray):
 	ci = []
 	centers = []
 	if len(contours) > 1:
+
+		# for each contour: calculate the area and filter the patches that are too small
 		for i in range(len(contours)):
 			cnt = contours[n]
 			area = cv2.contourArea(cnt)
@@ -38,6 +57,13 @@ def findDefects(frame, gray):
 
 			n += 1
 		
+		# for each contour left:
+		# - calculate the COG by using the moments of the contour (http)
+		# - Get the defects of the fingers
+		# - By defining the defect location: draw lines between the fingertips
+		# 	creating a contour of the hand
+		# - Draw the contour of the hand on the frame
+
 		for i in ci:
 			cnt = contours[i]
 			M = cv2.moments(cnt)
@@ -58,6 +84,8 @@ def findDefects(frame, gray):
 	return centers
 			
 
+# Function to calculate the total offset given the current and previous centers, 
+# and the cumulative offset yet obtained
 def processHand(center, prev_center, prev_Offset):
 
 
@@ -70,7 +98,7 @@ def processHand(center, prev_center, prev_Offset):
 	return (off_X, off_Y) 
 
 
-
+# Functions for length processing
 def getLength(vector):
 	return (vector[0]**2 + vector[1]**2)**0.5
 
@@ -82,6 +110,9 @@ def getLengthY(vector):
 	return vector[1]
 
 
+
+# Function to get the right associations between two centers in two consecutive frames
+# (Only when two hands are in the frame)
 def getPreviousCenter(center, previous_centers):
 	i = 0
 	maxDistance = 10000000
